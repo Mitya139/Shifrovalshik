003b99790b60c43d989a73a5eede8a4c84650c9a
Mitya139 <kondratev.diu@phystech.edu>
2022-11-21 06:39:39 +0300

    Encryption and decryption of files and text

--- Inputing.py
+++ Inputing.py
@@ -10,6 +10,10 @@ def start_in_out():
         inputing_for_text.language_in_out()
     elif first_in == '2':
         inputing_files.end_way_to_file_in()
+    elif first_in == '3':
+        inputing_for_text.language_in_out(True)
+    elif first_in == '4':
+        inputing_files.end_way_to_file_in(True)
     else:
         print('Некорректный ввод')
         start_in_out()
--- inputing_for_text.py
+++ inputing_for_text.py
@@ -3,69 +3,80 @@ import Caesar
 import Visener
 
 
-def caesar_cipher_end(language, text, key):
+def caesar_cipher_end(language, text, key, decrypt=False):
     print('Зашифрованный текст:')
     if language == 'eng':
-        print(Caesar.encrypt_caesar_text_eng(text, key))
+        if decrypt:
+            print(Caesar.decrypt_caesar_text_eng(text, key))
+        else:
+            print(Caesar.encrypt_caesar_text_eng(text, key))
     elif language == 'rus':
-        print(Caesar.encrypt_caesar_text_rus(text, key))
+        if decrypt:
+            print(Caesar.decrypt_caesar_text_rus(text, key))
+        else:
+            print(Caesar.encrypt_caesar_text_rus(text, key))
 
 
-def visener_cipher_end(language, text, key):
+def visener_cipher_end(language, text, key, decrypt=False):
     print('Зашифрованный текст:')
     if language == 'eng':
-        print(Visener.encrypt_visener_text_eng(text, key))
+        if decrypt:
+            print(Visener.decrypt_visener_text_eng(text, key))
+        else:
+            print(Visener.encrypt_visener_text_eng(text, key))
     elif language == 'rus':
-        print(Visener.decrypt_visener_text_rus(text, key))
+        if decrypt:
+            print(Visener.decrypt_visener_text_rus(text, key))
+        else:
+            print(Visener.decrypt_visener_text_rus(text, key))
 
 
-def caesar_text_input(language):
+def caesar_text_input(language, decrypt=False):
     print('Введите текст:')
     text = input()
-    key_input_caesar_text(language, text)
+    key_input_caesar_text(language, text, decrypt)
 
 
-def visener_text_input(language):
+def visener_text_input(language, decrypt=False):
     print('Введите текст:')
     text = input()
     print('Введите ключ:')
     key = input()
-    visener_cipher_end(language, text, key)
+    visener_cipher_end(language, text, key, decrypt)
 
 
-def key_input_caesar_text(language, text):
+def key_input_caesar_text(language, text, decrypt=False):
     Outputing.key_output()
     key = input()
     try:
         key = int(key)
-        caesar_cipher_end(language, text, key)
+        caesar_cipher_end(language, text, key, decrypt)
     except ValueError:
         print('Некорректный ключ')
-        key_input_caesar_text(language, text)
+        key_input_caesar_text(language, text, decrypt)
 
 
-def cipher_text_choice(language):
+def cipher_text_choice(language, decrypt=False):
     Outputing.output_text()
     cipher_text_in = input()
-    text_for_encryption = ''
     if cipher_text_in == '1':
-        caesar_text_input(language)
+        caesar_text_input(language, decrypt)
     elif cipher_text_in == '2':
-        visener_text_input(language)
+        visener_text_input(language, decrypt)
     elif cipher_text_in == '3':
-        visener_text_input(language)
+        visener_text_input(language, decrypt)
     else:
         print('Некорректный ввод')
-        cipher_text_choice(language)
+        cipher_text_choice(language, decrypt)
 
 
-def language_in_out():
+def language_in_out(decrypt=False):
     Outputing.language_choice()
     language_in = input()
     if language_in == '1':
-        cipher_text_choice('rus')
+        cipher_text_choice('rus', decrypt)
     elif language_in == '2':
-        cipher_text_choice('eng')
+        cipher_text_choice('eng', decrypt)
     else:
         print('Некорректный ввод')
-        language_in_out()
+        language_in_out(decrypt)
